9c01315c5f6ebc14729a8d270737de8d90756ef9
Avinaash Ghansam <avinaashghansam@avinaashs-mbp.mynetworksettings.com>
2024-06-29 01:53:13 -0400

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  Image,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  Button,
+  Alert,
+  Platform,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("clicked...");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text onPress={handlePress}>Hello!!</Text>
+      <TouchableOpacity onPress={() => console.log("image touched")}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableOpacity>
+      <Button
+        title="click me"
+        onPress={() =>
+          Alert.alert("My Title", "This is an alert msg", [
+            { text: "Yes", onPress: () => console.log("yes") },
+            { text: "No", onPress: () => console.log("no") },
+          ])
+        }
+        color="orange"
+      />
+      {/*<StatusBar style="auto" />*/}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.length : 0,
   },
 });
